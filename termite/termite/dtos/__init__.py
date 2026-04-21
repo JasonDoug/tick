9373c491341a0c9b0151ae72fd618c8720b53632
JasonDoug <potter.jason@gmail.com>
2026-04-21 05:00:33 -0700

    cleanup

--- termite/termite/dtos/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from termite.dtos.Script import Script
-    from termite.dtos.Config import Config
-except ImportError:
-    from dtos.Script import Script
-    from dtos.Config import Config
--- termite/termite/shared/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from termite.shared.utils.fix_imports import fix_any_import_errors
-    from termite.shared.utils.python_exe import get_python_executable
-except ImportError:
-    from shared.utils.fix_imports import fix_any_import_errors
-    from shared.utils.python_exe import get_python_executable
